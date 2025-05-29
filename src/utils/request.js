6261fdcd29a1901efa3e05cfccc8b9c7c4fac961
shadi <shadi9987@gmail.com>
2025-05-30 01:17:54 +0200

    add Bak-end Domain

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const request=axios.create({
-    baseURL:"https://ziddni-back-end.vercel.app/"
+    baseURL:"https://ziddni-back-end.vercel.app"
 });
 
 export default request;
