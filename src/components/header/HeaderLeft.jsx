0ac2a1cf60c6b1a7bfd725bcf73e675f37b6c662
shadi <shadi9987@gmail.com>
2025-05-30 01:54:16 +0200

    add Bak-end Domain

--- src/components/header/HeaderLeft.jsx
+++ src/components/header/HeaderLeft.jsx
@@ -2,7 +2,7 @@ const HeaderLeft = ({toggel,setToggel}) => {
     return ( 
         <div className="header-left">
                 <div className="header-logo">
-                    <img  className="bi bi-logo" src="/images/logo.png" alt="" />
+                    <img  className="bi bi-logo" src="/Images/logo.png" alt="" />
                     <strong>Ziddni</strong>
                 </div>
                 <div onClick={()=>setToggel(prev=> !prev)} className="header-menu">
